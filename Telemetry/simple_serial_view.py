8ae068fcb86bf9449f02866a15790768c7f49e61
Tiraken@gmail.com <Tiraken@gmail.com@06f136d1-d091-2ae3-e27c-133c6a94ca8d>
2011-07-23 06:28:08 +0000

    Added more functionality to serial code.

--- Telemetry/simple_serial_view.py
+++ Telemetry/simple_serial_view.py
@@ -4,6 +4,14 @@ import json
 import glob
 import os
 
+START_BYTE = 0xE7
+ESCAPE_BYTE = 0x75
+
+START_CHAR = chr(START_BYTE)
+ESCAPE_CHAR = chr(ESCAPE_BYTE)
+
+serial_data = ''
+
 def load_can_descriptors():
     "Looks for all of the *.can.json files and compiles them into a list of descriptors"
     desc_sets = []
@@ -25,6 +33,7 @@ desc_sets = load_can_descriptors()
 
 
 def readMessage():
+    findStartChar()
     can_id, msg_len = getPreamble()
     for msg_src, id_dict in desc_sets:
         if can_id in id_dict:
@@ -38,7 +47,7 @@ def readMessage():
     data = readData(msg_len)
     interpreted_data = ()
     if fmt:
-        interpreted_data = struct.unpack("<"+fmt, data)
+        interpreted_data = struct.unpack(str("<"+fmt), data)
     
     printIndent("Message from %s" % msg_src, 0)
     printIndent("Message name: %s" % name, 2)
@@ -49,6 +58,7 @@ def readMessage():
         printIndent("Data: %s" % (interpreted_data,), 2)
 
 def debug():
+    findStartChar()
     can_id, msg_len = getPreamble()
     data = readData(msg_len)
     print msg_len, data
@@ -57,22 +67,57 @@ def printIndent(string, indent):
     print (indent * ' ' + string)
 
 def getPreamble():
-    preamble = struct.unpack("<H", preamble)
+    preamble = readPreamble()
+    preamble, = struct.unpack("<H", preamble)
     can_id = preamble >> 4
     msg_len = preamble & 0xF
     return can_id, msg_len
 
 def readPreamble():
-    preamble = ser.read(2)
+    preamble = read(2)
+    return preamble
+
+def read(len):
+    s = ''
+    for x in range(len):
+        s += getNextByteStr()
+    return s
+
+def getNextByteStr():
+    byteString = ser.read(1);
+    byte, = struct.unpack("<B", byteString)
+    if byte != ESCAPE_BYTE:
+        return chr(byte)
+    else:
+        b, = struct.unpack("<B", ser.read(1))
+        return chr(b ^ ESCAPE_BYTE)
 
 def readData(length):
-    return ser.read(length) if length else ''
+    return read(length)
+
+def findStartChar():
+    while read(1) != START_CHAR:
+        pass
+
+def hasCompleteMessage():
+    startMessagePos = serial_data.find(START_CHAR)
+    nextStartMessagePos = serial_data.find(START_CHAR, startMessagePos+1)
+    return startMessagePos != nextStartMessagePos
+
+def pollSerialData():
+    serial_data += ser.read(10000)
 
 if __name__ == '__main__':
-    ser = serial.Serial()
+    ser = serial.Serial(timeout=0)
     ser.baudrate = 115200
-    ser.port = 'COM1'
+    ser.port = 'COM4'
+    ser.bytesize = 8
+    ser.parity = 'N'
+    ser.stopbits = 1
     ser.open()
+##    print    ser.read(100)
     while True:
-##        readMessage()
-        debug()
+#        pollSerialData()
+#        if hasCompleteMessage():
+            readMessage()
+##        debug()
